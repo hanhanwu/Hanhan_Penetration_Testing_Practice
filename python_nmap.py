290522a499f8bf8ebf1d1db054dea1921fa6ada8
hanhan <hanhanwu@users.noreply.github.com>
2017-12-15 23:58:08 -0800

    update python_nmap.py

--- python_nmap.py
+++ python_nmap.py
@@ -1,5 +1,90 @@
 import nmap
+import netifaces
 
-nm = nmap.PortScanner()
-nm.scan('127.0.0.1', '79')  # scan 1 port
-nm.scan('127.0.0.1', '77-99')  # scan port range
+scanner = nmap.PortScanner()
+scanner.scan('127.0.0.1', '79')  # scan 1 port
+scanner.scan('127.0.0.1', '77-99')  # scan port range
+
+for host in scanner.all_hosts():
+    print scanner[host]  # print every port info in json format
+    print scanner[host].hostname()
+    print scanner[host].all_protocols()
+    
+print scanner.csv() # print every port info row by row
+
+home_dir="/root"
+gateways = {}
+network_ifaces={}
+
+# Get network interface
+def get_interfaces():
+    interfaces = netifaces.interfaces()
+    return interfaces
+  
+# Get network gateways
+def get_gateways():
+    gateway_dict = {}
+    gws = netifaces.gateways()
+    for gw in gws:
+        try:
+            gateway_iface = gws[gw][netifaces.AF_INET]
+            gateway_ip, iface = gateway_iface[0], gateway_iface[1]
+            gw_list =[gateway_ip, iface]
+            gateway_dict[gw]=gw_list
+        except:
+            pass
+    return gateway_dict
+  
+# Get hwaddr, iface_addr, iface_broadcast, iface_netmask
+def get_addresses(interface):
+    addrs = netifaces.ifaddresses(interface)
+    link_addr = addrs[netifaces.AF_LINK]
+    iface_addrs = addrs[netifaces.AF_INET]
+    iface_dict = iface_addrs[0]
+    link_dict = link_addr[0]
+    hwaddr = link_dict.get('addr')
+    iface_addr = iface_dict.get('addr')
+    iface_broadcast = iface_dict.get('broadcast')
+    iface_netmask = iface_dict.get('netmask')
+    return hwaddr, iface_addr, iface_broadcast, iface_netmask
+
+# For each gateway, get hwaddr, iface_addr, iface_broadcast, iface_netmask
+def get_networks(gateways_dict):
+    networks_dict = {}
+    for key, value in gateways.iteritems():
+        gateway_ip, iface = value[0], value[1]
+        hwaddress, addr, broadcast, netmask = get_addresses(iface)
+        network = {'gateway': gateway_ip, 'hwaddr' : hwaddress, 'addr' : addr, '                                                                                        broadcast' : broadcast, 'netmask' : netmask}
+        networks_dict[iface] = network
+    return networks_dict
+  
+# Collect host (those are not your host) with open port
+def target_identifier(dir,user,passwd,ips,port_num,ifaces):
+    bufsize = 0
+    ssh_hosts = "%s/ssh_hosts" % (dir)
+    scanner = nmap.PortScanner()
+    scanner.scan(ips, port_num)
+    open(ssh_hosts, 'w').close()
+    if scanner.all_hosts():
+        e = open(ssh_hosts, 'a', bufsize)
+    else:
+        sys.exit("[!] No viable targets were found!")
+    for host in scanner.all_hosts():
+        for k,v in ifaces.iteritems():
+            if v['addr'] == host:
+                print("[-] Removing %s from target list since it belongs to your interface!") % (host)
+                host = None
+        if host != None:
+            home_dir="/root"
+            ssh_hosts = "%s/ssh_hosts" % (home_dir)
+            bufsize=0
+            e = open(ssh_hosts, 'a', bufsize)
+            if 'ssh' in scanner[host]['tcp'][int(port_num)]['name']:
+                if 'open' in scanner[host]['tcp'][int(port_num)]['state']:
+                    print("[+] Adding host %s to %s since the service is active on %s") % (host,ssh_hosts,port_num)
+                    hostdata=host + "\n"
+                    e.write(hostdata)
+    if not scanner.all_hosts():
+        e.closed
+    if ssh_hosts:
+        return ssh_hosts
